ba56ad5bd112d55d03b66365d29893d3ec370389
Hyunsoo <rex8312@gmail.com>
2022-09-12 19:38:02 +0900

    .

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -57,11 +57,11 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+# html_theme = "alabaster"
 # html_theme = "sphinx_rtd_theme"
-# html_theme_path = [
-#     "_themes",
-# ]
+html_theme = "sphinx_book_theme"
+# html_theme = "press"
+html_theme_path = ["_themes"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
